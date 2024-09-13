6074d5155dc7ba6b0fb92c3aecefd52320c934a4
silva4dev <web4dev.lucas@outlook.com>
2024-09-12 21:02:23 -0300

    feat: :necktie: add students repository

---
+++ src/application/repositories/students_repository.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Application
+  module Repositories
+    class StudentsRepository
+      def find_by_id(id)
+        raise NotImplementedError
+      end
+    end
+  end
+end
