7e23a818cf6031697fbe4ce7a78acd74473ce0ca
silva4dev <web4dev.lucas@outlook.com>
2024-09-12 21:02:12 -0300

    feat: :necktie: add challenges repository

---
+++ src/application/repositories/challenges_repository.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Application
+  module Repositories
+    class ChallengesRepository
+      def find_by_id(id)
+        raise NotImplementedError
+      end
+    end
+  end
+end
