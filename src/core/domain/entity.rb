ce31fee9d9cb2909a78c26a005098b33b5507ce0
silva4dev <web4dev.lucas@outlook.com>
2024-09-12 21:02:57 -0300

    feat: :necktie: add entity base

---
+++ src/core/domain/entity.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'uuid'
+
+module Core
+  module Domain
+    class Entity
+      attr_reader :id
+      attr_accessor :props
+
+      def initialize(props, id = nil)
+        @props = props
+        @id = id || UUID.new.generate
+      end
+    end
+  end
+end
