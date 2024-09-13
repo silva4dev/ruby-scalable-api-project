37973bef8b790b9b60f8cf3905220128e8b6dd33
silva4dev <web4dev.lucas@outlook.com>
2024-09-12 21:03:08 -0300

    feat: :necktie: add challenge entity

---
+++ src/domain/entities/challenge.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative '../../core/domain/entity'
+
+module Domain
+  module Entities
+    class Challenge < Core::Domain::Entity
+      private_class_method :new
+
+      def initialize(props, id = nil)
+        super(props, id)
+      end
+
+      def self.create(props, id = nil)
+        new(props, id)
+      end
+    end
+  end
+end
