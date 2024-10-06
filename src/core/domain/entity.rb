253f8cb72b11aabea5b1cd0e404b4cf53ea0cbef
silva4dev <web4dev.lucas@outlook.com>
2024-10-05 21:51:21 -0300

    feat: :sparkles: add typing to files using sorbet

--- src/core/domain/entity.rb
+++ src/core/domain/entity.rb
@@ -1,14 +1,21 @@
 # typed: true
 # frozen_string_literal: true
 
+require 'sorbet-runtime'
 require 'uuid'
 
 module Core
   module Domain
     class Entity
+      extend T::Sig
+
+      sig { returns(String) }
       attr_reader :id
+
+      sig { returns(T::Hash[String, T.untyped]) }
       attr_accessor :props
 
+      sig { params(props: T::Hash[String, T.untyped], id: T.nilable(String)).void }
       def initialize(props, id = nil)
         @props = props
         @id = id || UUID.new.generate
